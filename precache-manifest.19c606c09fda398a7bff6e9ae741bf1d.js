1bbb3781d626271bec71f74c045ca1d14a8ef2a1
Mariano Conti <nanexcool@gmail.com>
2019-08-25 09:04:31 +0200

    Updates

---
+++ precache-manifest.19c606c09fda398a7bff6e9ae741bf1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f87bfc0a252b4eef51b41afbdee36ac3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7f40be413e81bb0cb6e",
+    "url": "/static/css/main.db1d0eec.chunk.css"
+  },
+  {
+    "revision": "046261fffa2ce89df99b",
+    "url": "/static/js/2.26d307e8.chunk.js"
+  },
+  {
+    "revision": "c7f40be413e81bb0cb6e",
+    "url": "/static/js/main.e7ff7070.chunk.js"
+  },
+  {
+    "revision": "087007a86a11f0fff7a4",
+    "url": "/static/js/runtime~main.29f3f7d8.js"
+  },
+  {
+    "revision": "ef8f1f8d4d486fba133e84dbfa93f180",
+    "url": "/static/media/moloch.ef8f1f8d.png"
+  }
+]);
